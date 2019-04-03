ff8ffafe3cc5b425b03f38fd35adb7d804d7b868
olyuninv <olyuninv@tcd.ie>
2019-04-03 09:55:10 +0100

    models

---
+++ Models/Girl01/materials.cs
@@ -0,0 +1,156 @@
+//--- Girl01.dae MATERIALS BEGIN ---
+singleton Material(Girl01_Girl01_UV_FMShortSleevesShirt)
+{
+	mapTo = "Girl01_UV_FMShortSleevesShirt";
+
+	diffuseMap[0] = "Girl01_Torso";
+	normalMap[0] = "";
+	specularMap[0] = "";
+
+	diffuseColor[0] = "1 1 1 1";
+	specular[0] = "0 0 0 1";
+	specularPower[0] = 2;
+
+	doubleSided = false;
+	translucent = false;
+	translucentBlendOp = "None";
+};
+
+singleton Material(Girl01_Girl01_Hair)
+{
+	mapTo = "Girl01_Hair";
+
+	diffuseMap[0] = "";
+	normalMap[0] = "";
+	specularMap[0] = "";
+
+	diffuseColor[0] = "0.788235 0.376471 0.898039 1";
+	specular[0] = "0 0 0 1";
+	specularPower[0] = 2;
+
+	doubleSided = false;
+	translucent = false;
+	translucentBlendOp = "None";
+};
+
+singleton Material(Girl01_EarBake)
+{
+	mapTo = "EarBake";
+
+	diffuseMap[0] = "Ears_AnimeHeadParts";
+	normalMap[0] = "";
+	specularMap[0] = "";
+
+	diffuseColor[0] = "1 1 1 1";
+	specular[0] = "0 0 0 1";
+	specularPower[0] = 2;
+
+	doubleSided = false;
+	translucent = false;
+	translucentBlendOp = "None";
+};
+
+singleton Material(Girl01_Girl01_HeadBake)
+{
+	mapTo = "Girl01_HeadBake";
+
+	diffuseMap[0] = "Girl01_Head";
+	normalMap[0] = "";
+	specularMap[0] = "";
+
+	diffuseColor[0] = "1 1 1 1";
+	specular[0] = "0 0 0 1";
+	specularPower[0] = 2;
+
+	doubleSided = false;
+	translucent = false;
+	translucentBlendOp = "None";
+};
+
+singleton Material(Girl01_Girl01_LEye)
+{
+	mapTo = "Girl01_LEye";
+
+	diffuseMap[0] = "Girl01_Eye";
+	normalMap[0] = "";
+	specularMap[0] = "";
+
+	diffuseColor[0] = "1 1 1 1";
+	specular[0] = "0 0 0 1";
+	specularPower[0] = 2;
+
+	doubleSided = false;
+	translucent = false;
+	translucentBlendOp = "None";
+};
+
+singleton Material(Girl01_Girl01_REye)
+{
+	mapTo = "Girl01_REye";
+
+	diffuseMap[0] = "Girl01_Eye";
+	normalMap[0] = "";
+	specularMap[0] = "";
+
+	diffuseColor[0] = "1 1 1 1";
+	specular[0] = "0 0 0 1";
+	specularPower[0] = 2;
+
+	doubleSided = false;
+	translucent = false;
+	translucentBlendOp = "None";
+};
+
+singleton Material(Girl01_Girl01_EyeBrow)
+{
+	mapTo = "Girl01_EyeBrow";
+
+	diffuseMap[0] = "";
+	normalMap[0] = "";
+	specularMap[0] = "";
+
+	diffuseColor[0] = "0 0 0 1";
+	specular[0] = "0 0 0 1";
+	specularPower[0] = 2;
+
+	doubleSided = false;
+	translucent = false;
+	translucentBlendOp = "None";
+};
+
+singleton Material(Girl01_Girl01_UV_ShortPants)
+{
+	mapTo = "Girl01_UV_ShortPants";
+
+	diffuseMap[0] = "Girl01_Legs";
+	normalMap[0] = "";
+	specularMap[0] = "";
+
+	diffuseColor[0] = "1 1 1 1";
+	specular[0] = "0 0 0 1";
+	specularPower[0] = 2;
+
+	doubleSided = false;
+	translucent = false;
+	translucentBlendOp = "None";
+};
+
+singleton Material(Girl01_Girl01_UV_Heels)
+{
+	mapTo = "Girl01_UV_Heels";
+
+	diffuseMap[0] = "Girl01_Feet";
+	normalMap[0] = "";
+	specularMap[0] = "";
+
+	diffuseColor[0] = "1 1 1 1";
+	specular[0] = "0 0 0 1";
+	specularPower[0] = 2;
+
+	doubleSided = false;
+	translucent = false;
+	translucentBlendOp = "None";
+};
+
+//--- Girl01.dae MATERIALS END ---
+
